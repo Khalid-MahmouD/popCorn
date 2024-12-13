5e6508e01414d95930cb833f9869588baa5af79d
Khalid-MahmouD <khaloooood827@gmail.com>
2024-12-13 13:51:45 +0200

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ export default function App() {
   // const [watched, setWatched] = useState(
   //   () => JSON.parse(localStorage.getItem("watched")) || [],
   // );
+
   function handleSelectMovie(id) {
     setSelectedId((prev) => (prev === id ? null : id));
   }
